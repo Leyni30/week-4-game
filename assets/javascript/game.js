6dd205e25b3967ecf3e9f6eb9bdfc6b66b8cd8f4
Elena <elena_donceva@yahoo.com>
2018-01-25 18:59:24 -0800

    last commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,94 @@
-$(document).ready(function(){
+// $(document).ready(function(){
+//  var wins = 0;
+//         var losses = 0;
+//         var myScore = 0;
+//         var randomNumber;
+//         var pic1 = 0;
+//         var pic2 = 0;
+//         var pic3 = 0;
+//         var pic4 = 0;
+//         var min;
+//         var max;
 
- var wins = 0;
-     var losses = 0;
-     var myScore = 0;
-     var randomNumber 
-     var pic1 = 0;
-     var pic2 = 0;
-     var pic3 = 0;
-     var pic4 = 0;
-     var min
-     var max 
+//         function getANumber(min, max) {
+//             randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+//         }
+//         getANumber(19, 120);
 
-    function getANumber(min, max) {
-        randomNumber =  Math.floor(Math.random() * (max - min + 1) + min);
-        console.log(randomNumber);
-}
-getANumber(19,120);
+//         function showNumber() {
+//             $("#ComputerNumber").html(randomNumber);
+//         }
 
-    function showNumber(){
-$("#ComputerNumber").html(randomNumber);
-    }
+//         showNumber();
 
-    showNumber();
-
-
-        function showScore() {
-         
-         var number =  Math.floor(Math.random() * (12 - 1 + 1) + 1);
-         //console.log(number);
-         return number;    
-        }
-
-        showScore();
+//         function appendStats(){
+//             var myDiv = "<div><p>Wins: "+ wins + "</p><p>Losses: " + losses +"</p></div>"
+//             $("#stats").html(myDiv)
+//         }
+//         appendStats();
 
 
-        function refreshPics(){
+//         function showScore() {
 
-        pic1 = showScore();
-        pic2 = showScore();
-        pic3 = showScore();
-        pic4 = showScore();
+//             var number = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+//             //console.log(number);
+//             return number;
+//         }
 
-           }
-
-           function updating(){
-
-            $("#score").html(myScore);
-           }
-        
-
-        refreshPics();
+//         showScore();
 
 
-        $("#pic1").click(function(){
-            myScore += pic1;
-            console.log(myScore);
-            updating();
-        });
+//         function refreshPics() {
 
-        $("#pic2").click(function(){
-            myScore += pic2;
-            updating();
-        });
+//             pic1 = showScore();
+//             pic2 = showScore();
+//             pic3 = showScore();
+//             pic4 = showScore();
 
-        $("#pic3").click(function(){
-            myScore += pic3;
-            updating();
-        });
-        $("#pic4").click(function(){
-            myScore += pic4;
-            updating();
-        });
-});
+//         }
+
+//         function updating() {
+
+//             $("#score").html(myScore);
+//         }
+
+
+//         refreshPics();
+
+
+//         $("#pic1").click(function() {
+//             myScore += pic1;
+//             console.log(myScore);
+//             updating();
+//         });
+
+//         $("#pic2").click(function() {
+//             myScore += pic2;
+//             updating();
+//         });
+
+//         $("#pic3").click(function() {
+//             myScore += pic3;
+//             updating();
+//         });
+//         $("#pic4").click(function() {
+//             myScore += pic4;
+//             updating();
+//         });
+
+
+//         function giveFinalResult() {
+//             if (myScore === randomNumber) {
+//                 wins++;
+//                 appendStats();
+//                 $(".win-counter").html(wins);
+
+//             } else if (myScore > randomNumber) {
+//                 losses++;
+//                 appendStats();
+//                 $(".loss-counter").html(losses);
+
+//             }
+//         }
+//         giveFinalResult();
+// });
